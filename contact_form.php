40f5c1a4606a16a324976297734bf839b8ac04e4
mikailtal <jeevanpunalur@gmail.com>
2022-12-11 20:38:57 +0530

    third

--- contact_form.php
+++ contact_form.php
@@ -1,6 +1,6 @@
 <?php
 
-$mail_to  = "jeevanpualur@gmail.com";  // replace it with your email
+$mail_to  = "jeevanpunalur@gmail.com";  // replace it with your email
 
 $name     = $_POST['name'];
 $email    = $_POST['email'];
